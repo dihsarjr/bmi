fe236113dfb3760acc734a568a67cbc8142ff14a
rashid <mohammed1rashid786@gmail.com>
2021-03-21 21:17:41 +0530

    function

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meters: UILabel!
+    @IBOutlet weak var weight: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        meters.text = String(sender.value)
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weight.text = String(sender.value)
+    }
 }
 
